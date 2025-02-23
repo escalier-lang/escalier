8e14ad762ab7cf55b45415ab6114bed009996f51
Kevin Barabash <kevinb7@gmail.com>
2025-02-23 01:42:35 -0500

    Convert existing parser tests to table-driven tests

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -7,316 +7,121 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParsingStringLiteral(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "\"hello\"",
+func TestParseExprNoErrors(t *testing.T) {
+	tests := map[string]struct {
+		input string
+	}{
+		"StringLiteral": {
+			input: "\"hello\"",
+		},
+		"NumberLiteral": {
+			input: "5",
+		},
+		"Addition": {
+			input: "a + b",
+		},
+		"AddSub": {
+			input: "a - b + c",
+		},
+		"MulAdd": {
+			input: "a * b + c * d",
+		},
+		"MulDiv": {
+			input: "a / b * c",
+		},
+		"UnaryOps": {
+			input: "+a - -b",
+		},
+		"Parens": {
+			input: "a * (b + c)",
+		},
+		"Call": {
+			input: "foo(a, b, c)",
+		},
+		"CallPrecedence": {
+			input: "a + foo(b)",
+		},
+		"CurriedCall": {
+			input: "foo(a)(b)(c)",
+		},
+		"OptChainCall": {
+			input: "foo?(bar)",
+		},
+		"ArrayLiteral": {
+			input: "[1, 2, 3]",
+		},
+		"Member": {
+			input: "a.b?.c",
+		},
+		"MemberPrecedence": {
+			input: "a + b.c",
+		},
+		"Index": {
+			input: "a[base + offset]",
+		},
+		"IndexPrecedence": {
+			input: "a + b[c]",
+		},
+		"MultipleIndexes": {
+			input: "a[i][j]",
+		},
+		"OptChainIndex": {
+			input: "a?[base + offset]",
+		},
 	}
 
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			source := Source{
+				path:     "input.esc",
+				Contents: test.input,
+			}
 
-	snaps.MatchSnapshot(t, expr)
+			parser := NewParser(source)
+			expr, _ := parser.parseExpr()
+
+			snaps.MatchSnapshot(t, expr)
+			assert.Len(t, parser.errors, 0)
+		})
+	}
 }
 
-func TestParsingNumberLiteral(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "5",
+func TestParseExprErrorHandling(t *testing.T) {
+	tests := map[string]struct {
+		input string
+	}{
+		"IncompleteBinaryExpr": {
+			input: "a - b +",
+		},
+		"ExtraOperatorsInBinaryExpr": {
+			input: "a + * b",
+		},
+		"IncompleteCall": {
+			input: "foo(a,",
+		},
+		"IncompleteMember": {
+			input: "a + b.",
+		},
+		"IncompleteMemberOptChain": {
+			input: "a + b?.",
+		},
 	}
 
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			source := Source{
+				path:     "input.esc",
+				Contents: test.input,
+			}
 
-	snaps.MatchSnapshot(t, expr)
-}
+			parser := NewParser(source)
+			expr, _ := parser.parseExpr()
 
-func TestParsingAddition(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "a + b",
+			snaps.MatchSnapshot(t, expr)
+			assert.Len(t, parser.errors, 1)
+			snaps.MatchSnapshot(t, parser.errors[0])
+		})
 	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-}
-
-func TestParsingAddSub(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "a - b + c",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 0)
-}
-
-func TestParsingIncompleteBinaryExpr(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "a - b +",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 1)
-	snaps.MatchSnapshot(t, parser.errors[0])
-}
-
-func TestParsingExtraOperatorsInBinaryExpr(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "a + * b",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 1)
-	snaps.MatchSnapshot(t, parser.errors[0])
-}
-
-func TestParsingMulAdd(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "a * b + c * d",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 0)
-}
-
-func TestParsingMulDiv(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "a / b * c",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 0)
-}
-
-func TestParsingUnaryOps(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "+a - -b",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 0)
-}
-
-func TestParsingParens(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "a * (b + c)",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 0)
-}
-
-func TestParsingCall(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "foo(a, b, c)",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 0)
-}
-
-func TestParsingIncompleteCall(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "foo(a,",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 1)
-	snaps.MatchSnapshot(t, parser.errors[0])
-}
-
-func TestParsingCallPrecedence(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "a + foo(b)",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 0)
-}
-
-func TestParsingCurriedCall(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "foo(a)(b)(c)",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 0)
-}
-
-func TestParsingOptChainCall(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "foo?(bar)",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 0)
-}
-
-func TestParsingArrayLiteral(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "[1, 2, 3]",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 0)
-}
-
-func TestParsingMember(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "a.b?.c",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 0)
-}
-
-func TestParsingMemberPrecedence(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "a + b.c",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 0)
-}
-
-func TestParsingIncompleteMember(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "a + b.",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 1)
-	snaps.MatchSnapshot(t, parser.errors[0])
-}
-
-func TestParsingIncompleteMemberOptChain(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "a + b?.",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 1)
-	snaps.MatchSnapshot(t, parser.errors[0])
-}
-
-func TestParsingIndex(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "a[base + offset]",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 0)
-}
-
-func TestParsingIndexPrecedence(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "a + b[c]",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 0)
-}
-
-func TestParsingMultipleIndexes(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "a[i][j]",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 0)
-}
-
-func TestParsingOptChainIndex(t *testing.T) {
-	source := Source{
-		path:     "input.esc",
-		Contents: "a?[base + offset]",
-	}
-
-	parser := NewParser(source)
-	expr, _ := parser.parseExpr()
-
-	snaps.MatchSnapshot(t, expr)
-	assert.Len(t, parser.errors, 0)
 }
