8855bd928edb515c38f8af3ff8e4d9d95b3c6e38
Kevin Barabash <kevinb7@gmail.com>
2025-02-27 21:01:36 -0500

    tidy up the code in extensions.js, start using npm workspaces

--- packages/vscode-escalier/src/extension.ts
+++ packages/vscode-escalier/src/extension.ts
@@ -1,8 +1,3 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
-
 import * as path from "path";
 import { workspace, ExtensionContext } from "vscode";
 
@@ -16,32 +11,17 @@ import {
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-  // The server is implemented in node
-  //   const serverModule = context.asAbsolutePath(
-  //     path.join("server", "out", "server.js")
-  //   );
   const serverModule = context.asAbsolutePath(
     path.join("..", "..", "bin", "lsp-server")
   );
 
-  // If the extension is launched in debug mode then the debug server options are used
-  // Otherwise the run options are used
-  // const serverOptions: ServerOptions = {
-  // 	run: { module: serverModule, transport: TransportKind.ipc },
-  // 	debug: {
-  // 		module: serverModule,
-  // 		transport: TransportKind.ipc,
-  // 	},
-  // };
-
   const executable = {
     command: serverModule,
-    // options: { cwd: workspace. },
     args: ["--stdio"],
     transport: TransportKind.stdio,
   };
 
-  const serverOptions = {
+  const serverOptions: ServerOptions = {
     run: executable,
     debug: executable,
   };
