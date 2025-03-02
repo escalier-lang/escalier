38a05fa074ee52060e4d2c1c71a546d2b01cc045
Kevin Barabash <kevinb7@gmail.com>
2025-03-02 02:27:46 -0500

    exclude 'out' directories when running tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    exclude: [
+      '**/node_modules/**',
+      '**/out/**',
+    ],
     coverage: {
       exclude: [
         '**/node_modules/**',
