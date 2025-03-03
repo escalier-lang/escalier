1602d8d42ac2ad2552f9dd8b3b67fc5e58d2c50d
Kevin Barabash <kevinb7@gmail.com>
2025-03-03 00:21:08 -0500

    add sendRequest() method, patch _resume() to not throw

--- packages/lsp-client/src/index.ts
+++ packages/lsp-client/src/index.ts
@@ -35,10 +35,6 @@ class SimpleStream {
 const stdin = new SimpleStream();
 const stdout = new SimpleStream();
 
-stdout.on('data', (chunk) => {
-    console.log("stdout.on('data') - chunk =", chunk.toString());
-});
-
 const fsWrapper = {
     ...fs,
     read: (
@@ -47,7 +43,7 @@ const fsWrapper = {
         offset: number,
         length: number,
         position: number | null | undefined,
-        callback,
+        callback: (err: NodeJS.ErrnoException | null, bytesRead: number, buffer: Uint8Array<ArrayBufferLike>) => void,
     ) => {
         if (fd === 0) {
             const srcBuffer: Buffer = stdin.read();
@@ -69,7 +65,7 @@ const fsWrapper = {
         position: number | null | undefined,
         callback: (err: NodeJS.ErrnoException | null, written: number, buffer: Uint8Array<ArrayBufferLike>) => void,
     ) => {
-        console.log("fs.write - fd =", fd);
+        // TODO: also handle stderr
         if (fd === 1) {
             stdout.emit("data", Buffer.from(buffer));
             callback(null, length, buffer);
@@ -82,36 +78,70 @@ const fsWrapper = {
 const go = new Go(fsWrapper);
 const buffer = fs.readFileSync(path.join(__dirname, '../../../bin/lsp-server.wasm'));
 
-async function sleep(duration: number) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, duration);
+let requestID = 0;
+
+const serverEmitter = new EventEmitter();
+const resolvers = new Map<number, (value: any) => void>();
+
+// Receives messages from the server via STDOUT
+stdout.on('data', (chunk) => {
+    const message = chunk.toString("utf-8");
+    const headerRegex = /Content-Length: (\d+)/;
+    const payload = message.replace(headerRegex, "").trim();
+    const object = JSON.parse(payload);
+
+    // TODO: validate the the object being returned is a valid RPC JSON response
+    if (object.id != null) {
+        const resolver = resolvers.get(object.id);
+        if (resolver) {
+            resolver(object.result);
+        }
+    } else {
+        serverEmitter.emit(object.method, object.params);
+    }
+});
+
+async function sendRequest(method: "initialize", params: lsp.InitializeParams): Promise<lsp.InitializeResult>;
+async function sendRequest(method: "shutdown", params: null): Promise<void>;
+async function sendRequest(method: "exit", params: null): Promise<void>;
+async function sendRequest(method: "textDocument/didChange", params: lsp.DidChangeTextDocumentParams): Promise<void>;
+async function sendRequest(method: string, params: any) {
+    const initPaylod = JSON.stringify({
+        jsonrpc: "2.0",
+        id: requestID,
+        method,
+        params,
     });
+    const initMessage = `Content-Length: ${initPaylod.length}\r\n\r\n${initPaylod}`;
+
+    stdin.emit("data", Buffer.from(initMessage, 'utf-8'));
+    const promise1 = new Promise((resolve) => {
+        resolvers.set(requestID, resolve);
+    });
+    const response = await promise1;
+    requestID += 1;
+    return response;
 }
 
-WebAssembly.instantiate(buffer, go.importObject).then(async (result) => {
+async function main() {
+    const result = await WebAssembly.instantiate(buffer, go.importObject);
     go.run(result.instance);
 
     console.log("running...");
 
-    let requestID = 0;
+    serverEmitter.on("textDocument/publishDiagnostics", (params: lsp.PublishDiagnosticsParams) => {
+        console.log("Received diagnostics");
+        console.log(JSON.stringify(params, null, 4));
+    });
+
     const initParams: lsp.InitializeParams = {
         processId: process.pid,
         rootUri: "file:///home/user/project",
         capabilities: {},
     };
-    const initPaylod = JSON.stringify({
-        jsonrpc: "2.0",
-        id: requestID++,
-        method: "initialize",
-        params: initParams,
-    });
-    const initMessage = `Content-Length: ${initPaylod.length}\r\n\r\n${initPaylod}`;
-    stdin.emit("data", Buffer.from(initMessage, 'utf-8'));
-
-    await sleep(500);
-
-    const initResponse = stdout.read();
-    console.log("initialize response =", initResponse.toString());
+    const initResponse = await sendRequest("initialize", initParams);
+    console.log("Initial response");
+    console.log(JSON.stringify(initResponse, null, 4));
 
     const didChangeParams: lsp.DidChangeTextDocumentParams = {
         textDocument: {
@@ -122,42 +152,10 @@ WebAssembly.instantiate(buffer, go.importObject).then(async (result) => {
             { text: 'console.log("Hello, world!")\nval x =\n' }
         ]
     };
-    const didChangePayload = JSON.stringify({
-        jsonrpc: "2.0",
-        id: requestID++,
-        method: "textDocument/didChange",
-        params: didChangeParams,
-    });
-    console.log("didChangePayload =", didChangePayload);
-    const didChangeMessage = `Content-Length: ${didChangePayload.length}\r\n\r\n${didChangePayload}`;
+    await sendRequest("textDocument/didChange", didChangeParams);
 
-    stdin.emit("data", Buffer.from(didChangeMessage, 'utf-8'));
-   
-    await sleep(1000);
-    
-    console.log("---- RESPONSE ----");
-    while (true) {
-        const response = stdout.read();
-        if (!response) {
-            break;
-        }
-        console.log(response.toString());
-    }
-    // const didChangeResponse = stdout.read();
-    // console.log("didChangeResponse response =", didChangeResponse);
-    // console.log("didChangeResponse response =", didChangeResponse.toString());
+    await sendRequest("shutdown", null);
+    await sendRequest("exit", null);
+}
 
-    // Each request and respone have matching IDs
-    // We'll need a way to wait for a response for a given request ID so that we
-    // can have a nice API for sending requests and getting responses.
-
-    // const endpoint = new JSONRPCEndpoint(stdin, stdout);
-
-    // const value = await endpoint.send('initialize', {
-    //     processId: process.pid,
-    //     rootUri: 'file:///home/user/project',
-    //     capabilities: {},
-    // });
-
-    // console.log("initialize result =", value);
-});
+main();
