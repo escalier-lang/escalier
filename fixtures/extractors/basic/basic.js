1ea2eb4025f0fbcb74f67a99f19f97b818f9573e
Kevin Barabash <kevinb7@gmail.com>
2025-04-04 00:18:03 -0400

    add fixtures

---
+++ fixtures/extractors/basic/basic.js
@@ -0,0 +1,4 @@
+const subject = C("hello");
+const [temp1] = InvokeCustomMatcherOrThrow(C, subject, undefined);
+const msg = temp1;
+//# sourceMappingURL=./basic.esc.map
---
+++ fixtures/extractors/rest_arg/rest_arg.js
@@ -0,0 +1,5 @@
+const subject = C("hello", 5, true);
+const [temp1, ...temp2] = InvokeCustomMatcherOrThrow(C, subject, undefined);
+const x = temp1;
+const y = temp2;
+//# sourceMappingURL=./rest_arg.esc.map
