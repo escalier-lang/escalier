d099e66900209ba22f87af3ea869c67c978b7273
Kevin Barabash <kevinb7@gmail.com>
2025-05-04 13:07:32 -0400

    fix tests

--- fixtures/functions/destructuring/destructuring.js
+++ fixtures/functions/destructuring/destructuring.js
@@ -1,9 +1,11 @@
+type;
+Obj  {a: number, b: string, c: boolean;
 const foo = function (temp1) {
   const {a, b, ...rest} = temp1;
-  return rest.c;
+  return rest;
 };
 function bar(temp2) {
   const {a, b, ...rest} = temp2;
-  return rest.c;
+  return rest;
 }
 //# sourceMappingURL=./destructuring.esc.map
--- fixtures/functions/rest_params/rest_params.js
+++ fixtures/functions/rest_params/rest_params.js
@@ -1,9 +1,1 @@
-const foo = function (...temp1) {
-  const args = temp1;
-  return args.length;
-};
-function bar(...temp2) {
-  const args = temp2;
-  return args.length;
-}
 //# sourceMappingURL=./rest_params.esc.map
