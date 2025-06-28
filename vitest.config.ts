0a7404d1cb2ceb421b1f65f23831a9e5fba13509
Kevin Barabash <kevinb7@gmail.com>
2025-06-28 05:56:35 -0400

    Fix TypeScript coverage report (#90)

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,15 +4,7 @@ export default defineConfig({
     test: {
         exclude: ['**/node_modules/**', '**/out/**'],
         coverage: {
-            exclude: [
-                '**/node_modules/**',
-                '**/examples/**',
-                '**/fixtures/**',
-                '**/*.test.ts',
-                'playground/**',
-                'packages/vscode-escalier/**',
-                'vitest.config.ts',
-            ],
+            include: ['playground/src/lsp-client/**'],
         },
     },
 });
