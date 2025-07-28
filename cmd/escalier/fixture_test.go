106e9c36546aff842fb8dbf8df2a4c00dda82e64
Kevin Barabash <kevinb7@gmail.com>
2025-07-28 00:11:29 -0400

    re-enable all of the tests except for the extractor tests

--- cmd/escalier/fixture_test.go
+++ cmd/escalier/fixture_test.go
@@ -179,7 +179,7 @@ func TestBuildFixtureTests(t *testing.T) {
 
 		for _, fixture := range fixtures {
 			// TODO: use an environment variable for this instead
-			if group.Name() != "basics" && fixture.Name() != "literals" {
+			if group.Name() == "extractors" {
 				continue
 			}
 			name := group.Name() + "/" + fixture.Name()
