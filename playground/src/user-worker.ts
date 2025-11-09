b76d180beab6c414d8782d2631401867932c8367
Kevin Barabash <kevinb7@gmail.com>
2025-11-08 20:07:28 -0500

    fix TypeScript error in playground, optimize playground build

--- playground/src/user-worker.ts
+++ playground/src/user-worker.ts
@@ -1,12 +1,9 @@
-import * as monaco from 'monaco-editor';
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+import editorWorker from 'monaco-editor-core/esm/vs/editor/editor.worker?worker';
 
 // The editor worker is what runs the non-language specific parts of the editor.
 // We need to provide monaco-editor an instance of it when it asks for it.
-self.MonacoEnvironment = {
+(self as any).MonacoEnvironment = {
     getWorker(_moduleId: string, _label: string) {
         return new editorWorker();
     },
 };
-
-monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true);
